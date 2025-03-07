5a56786b9d3683ce39b5b03b40e5674fe753ac45
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2025-03-07 22:08:32 +0100

    Add command line parsing functionality and update project structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#define VERSION_STRING "0.1"
+
+#include "command_line_class.h"
+
+// Defineren aller Kommandozeilen Parameter
+enum CMD_COMMAND {CMD_HELP,CMD_VERSION, CMD_ANALYZE};
+static const CMD_STRUCT command_list[]{
+    {CMD_ANALYZE, "a", "analyze", "Analyzes the TAP file.", 1},
+    {CMD_HELP, "?", "help", "This text.", 0},
+    {CMD_VERSION, "", "version", "Displays the current version number.", 0}
+};
+
+#define command_list_count sizeof(command_list) / sizeof(command_list[0])
+
+CommandLineClass *cmd;
+
+int main(int argc, char *argv[]) 
+{
+    cmd = new CommandLineClass(argc, argv, "c64_tap_tool", command_list, command_list_count);
+
+    if(cmd->GetCommandCount() <= 0)
+    {
+        printf("\"c64_tap_tool --help\" provides more information.\n");
+        return(-1);
+    }
+
+    if(cmd->GetCommandCount() > 0)
+    {
+        for(int i=0; i<cmd->GetCommandCount(); i++)
+        {
+            if(cmd->GetCommand(i) == CMD_ANALYZE)
+            {
+                if(cmd->GetCommandCount() > i+1)
+                {
+                    const char *tap_file = cmd->GetArg(i+1);
+                    printf("Analyzing TAP file: %s\n",tap_file);
+                }
+                else
+                {
+                    printf("Missing TAP file.\n");
+                    return(-1);
+                }
+            }
+        }
+
+        if(cmd->FoundCommand(CMD_HELP))
+        {
+            cmd->ShowHelp();
+            return 0;
+        }
+
+        if(cmd->GetCommand(0) == CMD_VERSION)
+        {
+            printf("C64 TAP Tool - Version: %s\n\n",VERSION_STRING);
+            return(0x0);
+        }
+    }
+
     return 0;
 }
