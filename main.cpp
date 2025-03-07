196e2be8b035d955e50f984083eadda1b5966c9d
Thorsten Kattanek <thorsten.kattanek@gmx.de>
2025-03-08 00:42:32 +0100

    Implement TAP file validation and add initial documentation

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #define VERSION_STRING "0.1"
 
 #include "command_line_class.h"
+#include <string.h>
 
 void AnalyzeTAPFile(const char *tap_file);
 
@@ -65,6 +66,23 @@ int main(int argc, char *argv[])
     return 0;
 }
 
+bool IsTAPFile(uint8_t *data, uint32_t size)
+{
+    // Prüfe ob am Anfang der Datei C64-TAPE_RAW steht
+    const char header[] = "C64-TAPE-RAW";
+    if(memcmp(data, header, sizeof(header)-1) != 0)
+    {
+        return false;
+    }
+
+    return true;
+}
+
+int FoundSync(uint8_t *data, uint32_t size)
+{
+     return 0;
+}
+
 void AnalyzeTAPFile(const char *tap_file)
 {
     std::ifstream tap_file_stream(tap_file, std::ios::binary);
@@ -74,11 +92,21 @@ void AnalyzeTAPFile(const char *tap_file)
         std::streamoff file_size = tap_file_stream.tellg();
         tap_file_stream.seekg(0, std::ios::beg);
 
-        char *tap_data = new char[file_size];
-        tap_file_stream.read(tap_data, file_size);
+        uint8_t *tap_data = new uint8_t[file_size];
+        tap_file_stream.read((char*)tap_data, file_size);
         tap_file_stream.close();
 
         printf("TAP file size: %ld\n", static_cast<long>(file_size));
+        
+        if(IsTAPFile(tap_data, (uint32_t)file_size))
+        {
+            printf("TAP file is valid.\n");
+            FoundSync(tap_data, (uint32_t)file_size);
+        }
+        else
+        {
+            printf("TAP file is invalid.\n");
+        }
 
         delete[] tap_data;
     }
